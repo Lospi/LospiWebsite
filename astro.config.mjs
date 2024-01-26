63a16f96c6cc20da567e1d3db1bd9b0aa811124c
Roberto <roberto3154_421@hotmail.com>
2024-01-26 16:08:05 -0300

    feat: new website

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,11 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  buildOptions: {
+    // ... other build options
+    site: {
+      assets: '/AAT_WEB', // Adjust this path to match your folder structure
+    },
+  },
 });
