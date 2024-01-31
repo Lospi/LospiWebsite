0834de4ea12c62beb1390170a5f97eda9a4601ab
Roberto <roberto3154_421@hotmail.com>
2024-01-31 09:51:35 -0300

    feat: add routing

---
+++ lib/outerspaces.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OuterSpaces extends StatelessWidget {
+  const OuterSpaces({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(child: Text("Outer Spaces")),
+    );
+  }
+}
